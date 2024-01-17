01534c5c5aed3b91bdd5d8e5c7fd4cf41584905a
Chris McCambridge <chrissiemcc44@gmail.com>
2024-01-17 20:53:57 +0000

    Updated inheritance code for elemental staves

--- src/main/java/net/apixelmelon/elementsmod/item/custom/StaffItem.java
+++ src/main/java/net/apixelmelon/elementsmod/item/custom/StaffItem.java
@@ -1,9 +1,35 @@
 package net.apixelmelon.elementsmod.item.custom;
 
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public class StaffItem extends Item {
     public StaffItem(Properties pProperties) {
         super(pProperties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pUsedHand);
+
+        pPlayer.getCooldowns().addCooldown(this, 40); // Staff projectile cooldown
+
+        specifyProjectile(pLevel, pPlayer); // Unique staff classes override this method
+
+        pPlayer.awardStat(Stats.ITEM_USED.get(this));
+        if (!pPlayer.getAbilities().instabuild) {
+            itemstack.hurtAndBreak(1, pPlayer, p -> p.broadcastBreakEvent(pUsedHand));
+        }
+
+        return InteractionResultHolder.sidedSuccess(itemstack, pLevel.isClientSide());
+    }
+
+    public void specifyProjectile(Level pLevel, Player pPlayer) {
+
     }
 }
--- src/main/java/net/apixelmelon/elementsmod/item/custom/staves/WaterStaffItem.java
+++ src/main/java/net/apixelmelon/elementsmod/item/custom/staves/WaterStaffItem.java
@@ -2,11 +2,7 @@ package net.apixelmelon.elementsmod.item.custom.staves;
 
 import net.apixelmelon.elementsmod.entity.custom.WaterStaffProjectileEntity;
 import net.apixelmelon.elementsmod.item.custom.StaffItem;
-import net.minecraft.stats.Stats;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
 public class WaterStaffItem extends StaffItem {
@@ -15,22 +11,11 @@ public class WaterStaffItem extends StaffItem {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        ItemStack itemstack = pPlayer.getItemInHand(pUsedHand);
-
-        pPlayer.getCooldowns().addCooldown(this, 40); // Staff projectile cooldown
-
+    public void specifyProjectile(Level pLevel, Player pPlayer) {
         if(!pLevel.isClientSide()) {
             WaterStaffProjectileEntity waterStaffProjectile = new WaterStaffProjectileEntity(pLevel, pPlayer);
             waterStaffProjectile.shootFromRotation(pPlayer, pPlayer.getXRot(), pPlayer.getYRot(), 0.0F, 1.5F, 0.25F);
             pLevel.addFreshEntity(waterStaffProjectile);
         }
-
-        pPlayer.awardStat(Stats.ITEM_USED.get(this));
-        if (!pPlayer.getAbilities().instabuild) {
-            itemstack.hurtAndBreak(1, pPlayer, p -> p.broadcastBreakEvent(pUsedHand));
-        }
-
-        return InteractionResultHolder.sidedSuccess(itemstack, pLevel.isClientSide());
     }
 }
