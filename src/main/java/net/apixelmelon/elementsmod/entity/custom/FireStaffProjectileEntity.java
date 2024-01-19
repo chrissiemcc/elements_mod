5dd9025da50061d2ce9c561c704485bbe7f148a5
Chris McCambridge <chrissiemcc44@gmail.com>
2024-01-19 15:46:46 +0000

    Updated code

--- src/main/java/net/apixelmelon/elementsmod/entity/custom/FireStaffProjectileEntity.java
+++ src/main/java/net/apixelmelon/elementsmod/entity/custom/FireStaffProjectileEntity.java
@@ -7,8 +7,6 @@ import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
